636dc6cee87f0f68d47f08155d2796e0b1181781
Kevin Roberts <kevin.roberts@icfolson.com>
2016-10-04 13:37:11 -0500

    added logic for leaderboard listing

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var hbs = require('express-hbs');
 var path = require('path');
+require('dotenv').config({silent: true});
+var Slack = require('slack-node');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var firebase = require("firebase");
+
+if (!process.env.FIREBASEURI) {
+  console.log('Error: Specify Firebase configs in environment');
+  process.exit(1);
+}
+if (!process.env.TOKEN) {
+  console.log('Error: Specify Slack token in environment');
+  process.exit(1);
+}
+
+// initialize Firebase connection
+var config = {
+  apiKey: process.env.FIREBASEAPIKEY,
+  authDomain: process.env.FIREBASEURI,
+  databaseURL: process.env.FIREBASEDBURL,
+  storageBucket: process.env.FIREBASESTORAGE,
+  messagingSenderId: process.env.FIREBASEMSGID
+};
+firebase.initializeApp(config);
+
+var slack = new Slack(process.env.TOKEN);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -17,7 +41,24 @@ var app = express();
 // set specific global logic for production mode content
 app.locals.production = process.env.NODE_ENV === 'production';
 app.locals.currentyear = new Date().getFullYear();
+app.locals.firebase = firebase;
+app.locals.slack = slack;
+
 // view engine setup
+
+hbs.registerHelper("math", function(lvalue, operator, rvalue, options) {
+  lvalue = parseFloat(lvalue);
+  rvalue = parseFloat(rvalue);
+
+  return {
+    "+": lvalue + rvalue,
+    "-": lvalue - rvalue,
+    "*": lvalue * rvalue,
+    "/": lvalue / rvalue,
+    "%": lvalue % rvalue
+  }[operator];
+});
+
 app.engine('hbs', hbs.express4({
   defaultLayout: __dirname + '/views/layouts/default.hbs',
   layoutsDir: __dirname + '/views/layouts',
@@ -27,7 +68,7 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,9 +1,85 @@
+var _ = require('lodash');
 var express = require('express');
 var router = express.Router();
 
 /* GET leaderboard page. */
 router.get('/', function(req, res, next) {
-    res.render('leaderboard', { title: 'Doorman Mike - Leaderboard' });
+    var usersRef = req.app.locals.firebase.database().ref('users');
+
+    var returnedData = {};
+    returnedData.title = 'Doorman Mike - Leaderboard';
+
+    getSlackUsers(req.app.locals.slack, function (slackUsers) {
+       if (slackUsers != null) {
+           getUsersForLeaderboard(usersRef, function (users) {
+               var userData = [];
+               if (users != null) {
+                   _.forEach(users, function (user) {
+                       var userObj = {};
+                       userObj.id = user.id;
+                       var slackUserObj = _.find(slackUsers, {'id' : user.id });
+
+                       if (_.has(user, 'name')) {
+                           userObj.aka = user.name;
+                       }
+                       if (_.has(slackUserObj, 'name')) {
+                           userObj.name = '@' + slackUserObj.name;
+                       } else {
+                           userObj.name = '@' + user.id;
+                       }
+                       if (_.has(slackUserObj, 'profile')) {
+                           var profile = slackUserObj.profile;
+                           if (_.has(profile, 'image_192')) {
+                               userObj.image = slackUserObj.profile.image_192;
+                           } else {
+                               userObj.image = "http://placekitten.com/200/200";
+                           }
+                       }
+                       if (_.has(user, 'fists')) {
+                           userObj.fists = user.fists;
+                       } else {
+                           userObj.fists = 0;
+                       }
+
+                       userData.push(userObj);
+                   });
+                   userData = _.orderBy(userData, ['fists'], ['desc']);
+                   returnedData.users = userData;
+
+                   res.render('leaderboard', returnedData);
+
+               } else {
+                   res.render('leaderboard', returnedData);
+               }
+           });
+
+       } else {
+           res.render('leaderboard', returnedData);
+       }
+    });
+
 });
+
+function getUsersForLeaderboard(usersRef, callback) {
+    usersRef.once('value', function (snap) {
+        callback(snap.val());
+    }, function (err) {
+        // code to handle read error
+        console.log('error trying to retrieve users from fb', err);
+        callback(null);
+    });
+
+}
+
+function getSlackUsers (slackApi, callback) {
+    slackApi.api("users.list", function (err, res) {
+        if (res.members) {
+            callback(res.members);
+        } else {
+            console.log('error trying to retrieve users from slack', err);
+            callback(null);
+        }
+    });
+}
 
 module.exports = router;
