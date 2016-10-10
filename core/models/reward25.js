42a7b88649bd19e2b1d44688f8bed86f6ee31333
Kevin Roberts <kevin.roberts@icfolson.com>
2016-10-10 13:30:28 -0500

    update to reward 25

--- core/models/reward25.js
+++ core/models/reward25.js
@@ -11,20 +11,22 @@ function Reward25(slackAPI, fireUser, customText) {
 
 Reward25.prototype.run = function() {
     var _this = this;
+
+    var userLink = utils.getLinkFromUserId(_this.fireUser.id);
+
+    var rewardMessage = ':fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5:\n' + userLink +
+        ' is an AMAZING person! You should pay more attention to him/her.  :dancer: ';
+
+    if (_this.customText != '') {
+        rewardMessage += '\n' + "Also " + userLink + " wanted you to know this... " + _this.customText;
+    }
+
+    rewardMessage += '\n:fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5:';
+
     utils.getSlackUsers(this.slackAPI, function (slackUsers) {
         if (slackUsers != null) {
             _.forEach(slackUsers, function (user) {
                 if (user.id != _this.fireUser.id && !user.is_bot) {
-                    var userLink = utils.getLinkFromUserId(_this.fireUser.id);
-
-                    var rewardMessage = ':fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5:\n' + userLink +
-                        ' is an AMAZING person! You should pay more attention to him/her.  :dancer: ';
-
-                    if (_this.customText != '') {
-                        rewardMessage += '\n' + "Also " + userLink + " wanted you to know this... " + _this.customText;
-                    }
-
-                    rewardMessage += '\n:fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5:';
 
                     utils.postMessage(_this.slackAPI, user.id, rewardMessage);
 
@@ -34,13 +36,29 @@ Reward25.prototype.run = function() {
     });
 
 
-    var selfMessage = ':fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5:\n\n' +
-        'Congrats on redeeming your 25 fists reward bro!! I just messaged all your friends about it!';
-
-    selfMessage += '\n\n:fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5::fist::skin-tone-5:';
+    var selfMessage = 'Congrats on redeeming your 25 fist reward bro!! I just messaged all your friends your message.';
     selfMessage += '\n\n — Love :doorman: Mike';
 
-    utils.postMessage(this.slackAPI, this.fireUser.id, selfMessage);
+    var attachments = [
+        {   "color" : 'warning',
+            "title": "Your Message:",
+            "fallback" : "reward message text",
+            "text" : rewardMessage
+        }
+    ];
+
+    var attachmentString = JSON.stringify(attachments);
+    var payload = {
+        channel: this.fireUser.id,
+        as_user : true,
+        text: selfMessage,
+        attachments: attachmentString
+    };
+
+    this.slackAPI.api('chat.postMessage', payload, function (err, res) {
+        console.log(res);
+    });
+
 
 };
 
